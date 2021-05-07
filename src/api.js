984ff386809fedff7e1074b0a57913a1b30cce31
Rom Grk <romgrk.cc@gmail.com>
2021-05-07 10:08:12 -0400

    feat: refactor API

--- src/api.js
+++ src/api.js
@@ -1,40 +1,31 @@
 
-// note: api base url set in package.json proxy
+// Note: api base url set in package.json proxy
+const API_BASE = ''
 
-// start with separate GET and POST calls with minimal error handling, 
-// I can make it pretty later
 
-const apiGet = async (route) => {
-    return (fetch(`${route}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-    })
-      .then((res) => res.json())
-      .then((json) => {  
-        console.log({ apiGetResults: json });
-        return json;
-      })
-      .catch((err) => console.log(err)))
+export const info = () => apiGet("/info").then(r => r.response)
+
+
+// Helpers
+
+export function apiGet(route) {
+  return fetch(`${API_BASE}${route}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  })
+  .then(res => res.json())
 }
 
 
- const apiPost = async (route, body) => {
-    return (fetch(`${route}`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(body)
-    })
-      .then((res) => res.json())
-      .then((json) => {  
-        console.log({ apiPostResults: json });
-        return json;
-      })
-      .catch((err) => console.log(err)))
+export function apiPost(route, body) {
+  return fetch(`${API_BASE}${route}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
+  })
+  .then(res => res.json())
 }
-
-
-module.exports = { apiGet, apiPost }
