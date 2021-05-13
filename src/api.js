22f4d68c11438b151d1600e07644036ecbd48b9a
Gordon Krieger <gordon.krieger@gmail.com>
2021-05-13 17:47:19 -0400

    rm stray curly brackets

--- src/api.js
+++ src/api.js
@@ -10,15 +10,15 @@ export const individuals = (body) =>
   apiPost('/individuals', body).then((r) => r.response);
 
 // ** note **: two endpoints appear to be incorrectly swapped in the api:
-// /g_variants/{id}}/individuals should return all individuals with the particular variant given by the id
+// /g_variants/{id}/individuals should return all individuals with the particular variant given by the id
 // this doesn't work, though... instead it returns all *biosamples* with this variant
-// similarly /g_variants/{id}}/biosamples returns, instead of biosamples, a set of *individuals*
+// similarly /g_variants/{id}/biosamples returns, instead of biosamples, a set of *individuals*
 // so for now we'll swap the routes back here so at least the rest of our code reads correctly
 
 export const individualsByVariantId = (id, body) =>
-  apiPost(`/g_variants/${id}}/biosamples`, body).then((r) => r.response);
+  apiPost(`/g_variants/${id}/biosamples`, body).then((r) => r.response);
 export const biosamplesByVariantId = (id, body) =>
-  apiPost(`/g_variants/${id}}/individuals`, body).then((r) => r.response);
+  apiPost(`/g_variants/${id}/individuals`, body).then((r) => r.response);
 
 // Helpers
 
