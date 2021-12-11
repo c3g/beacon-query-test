5213baffee176a915f71d6c3960dbbe6c0463352
Gordon Krieger <gordon.krieger@gmail.com>
2021-12-10 19:03:32 -0500

    start improved donut

---
+++ src/components/ImprovedDonut.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ImprovedDonut = ({ data, width }) => {
+  // improved donut chart todo
+  // should have option to display chart data in a table
+  // tooltip
+};
+
+export default ImprovedDonut;
