ee8efcc914f8874cb3029ad08ddab20770598a32
Gordon Krieger <gordon.krieger@gmail.com>
2021-12-14 21:27:12 -0500

    add recharts for improvedDonut

--- src/components/ImprovedDonut.js
+++ src/components/ImprovedDonut.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import PieChart from 'recharts/es6/chart/PieChart';
+import Pie from 'recharts/es6/polar/Pie';
+import Cell from 'recharts/es6/component/Cell';
+import Curve from 'recharts/es6/shape/Curve';
+import Tooltip from 'recharts/es6/component/Tooltip';
+import Sector from 'recharts/es6/shape/Sector';
+import { polarToCartesian } from 'recharts/es6/util/PolarUtils';
 
 const ImprovedDonut = ({ data, width }) => {
   // improved donut chart todo
